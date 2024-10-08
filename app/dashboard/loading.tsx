ed976e4b5514657413ad342a48f73c286fe7030f
BEstaff <1912975308@qq.com>
2024-10-08 18:54:12 +0800

    finish 12 chapter

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+    // 加载骨架
+    return <DashboardSkeleton />;
+  }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        ppr: 'incremental', // 'incremental' value 允许您对特定路由采用 PPR。
+      },
+};
 
 export default nextConfig;
